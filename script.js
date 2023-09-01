9014873fc23a0ca0601bfae446073c4421d92253
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2023-08-31 23:42:35 -0300

    Timer funcionality

---
+++ script.js
@@ -0,0 +1,40 @@
+let sec = 0
+let min = 0
+let hour = 0
+let interval
+
+function twoDigits(number) {
+    if(number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
+
+
+function start() {
+    interval = setInterval(counter, 1000);
+}
+
+function stop() {
+    clearInterval(interval);
+}
+
+function reset() {
+    sec = 0;
+    min = 0;
+    hour = 0;
+    document.getElementById("counter").innerHTML = twoDigits(hour) + ':' + twoDigits(min) + ':' + twoDigits(sec);
+}
+
+function counter() {
+    sec++;
+    if(sec==60) {
+        sec = 0;
+        min++;
+        if(min==60) {
+            min = 0;
+            hour++;
+        }
+    }
+    document.getElementById("counter").innerHTML = twoDigits(hour) + ':' + twoDigits(min) + ':' + twoDigits(sec);
+}
